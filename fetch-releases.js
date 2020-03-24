021ce1fabf2451b7c17105468472162c1484d0b3
Shelley Vohr <shelley.vohr@gmail.com>
2020-03-24 10:59:57 -0700

    chore: lint with prettier

--- fetch-releases.js
+++ fetch-releases.js
@@ -1,15 +1,15 @@
-const fetch = require('node-fetch')
+const fetch = require('node-fetch');
 
-const releaseUrl = 'https://api.github.com/repos/electron/electron/releases'
+const releaseUrl = 'https://api.github.com/repos/electron/electron/releases';
 
-function fetchPage (n = 0, allReleases = []) {
+function fetchPage(n = 0, allReleases = []) {
   return fetch(`${releaseUrl}?per_page=100&page=${n}`)
-    .then(r => r.json())
-    .then(releases => {
-      if (releases.length === 0) return allReleases
-      allReleases.push(...releases)
-      return fetchPage(n + 1, allReleases)
-    })
+    .then((r) => r.json())
+    .then((releases) => {
+      if (releases.length === 0) return allReleases;
+      allReleases.push(...releases);
+      return fetchPage(n + 1, allReleases);
+    });
 }
 
-fetchPage().then(releases => console.log(JSON.stringify(releases, null, 2)))
+fetchPage().then((releases) => console.log(JSON.stringify(releases, null, 2)));
--- releases.js
+++ releases.js
@@ -1,16 +1,16 @@
-const releases = require('./releases.json').slice(0, 30)
+const releases = require('./releases.json').slice(0, 30);
 
-console.log('\n\n## Asset Downloads\n')
-console.log('> Raw download counts for every released GitHub asset.\n')
+console.log('\n\n## Asset Downloads\n');
+console.log('> Raw download counts for every released GitHub asset.\n');
 
-releases.forEach(release => {
-  console.log(`\n### ${release.tag_name}\n`)
+releases.forEach((release) => {
+  console.log(`\n### ${release.tag_name}\n`);
 
-  console.log('File | Downloads')
-  console.log('--- | ---')
+  console.log('File | Downloads');
+  console.log('--- | ---');
   release.assets
     .sort((a, b) => b.download_count - a.download_count)
-    .forEach(asset => {
-      console.log(`${asset.name} | ${asset.download_count}`)
-    })
-})
+    .forEach((asset) => {
+      console.log(`${asset.name} | ${asset.download_count}`);
+    });
+});
