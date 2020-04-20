ac2a8d72dbfdcdd1b50377c46094297526469ce4
Shelley Vohr <shelley.vohr@gmail.com>
2020-04-19 18:18:44 -0700

    refactor: sort and collapse data

--- releases.js
+++ releases.js
@@ -1,10 +1,17 @@
-const releases = require('./releases.json').slice(0, 30);
+const unsorted = require('./releases.json').slice(0, 30);
+const semver = require('semver');
 
 console.log('\n\n## Asset Downloads\n');
 console.log('> Raw download counts for every released GitHub asset.\n');
 
+const releases = unsorted.sort((r1, r2) => {
+  return semver.lte(r1.tag_name, r2.tag_name) ? 1 : -1;
+});
+
 releases.forEach((release) => {
   console.log(`\n### ${release.tag_name}\n`);
+
+  console.log('<details><summary>Download Data</summary>\n');
 
   console.log('File | Downloads');
   console.log('--- | ---');
@@ -13,4 +20,6 @@ releases.forEach((release) => {
     .forEach((asset) => {
       console.log(`${asset.name} | ${asset.download_count}`);
     });
+
+  console.log('\n</details>');
 });
