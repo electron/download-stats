e4a5c3190ad7b363eb63ed79c9341a25c8782a71
Shelley Vohr <shelley.vohr@gmail.com>
2024-12-04 15:42:39 +0100

    refactor: clean up combine logic

--- index.js
+++ index.js
@@ -9,17 +9,16 @@ const generateYearRange = () => {
   }, (_, i) => 2015 + i);
 };
 
-let combined = {};
-
-Object.keys(data).forEach((key) => {
-  data[key].downloads.forEach((metric) => {
-    const { day, downloads } = metric;
-    if (!combined[day]) combined[day] = 0;
-    combined[day] += downloads;
+let combined = Object.keys(data).reduce((acc, key) => {
+  data[key].downloads.forEach(({ day, downloads }) => {
+    acc[day] = (acc[day] || 0) + downloads;
   });
-});
+  return acc;
+}, {});
 
-combined = Object.entries(combined).sort((a, b) => a[0].localeCompare(b[0]));
+combined = Object.entries(combined).sort((a, b) => {
+  a[0].localeCompare(b[0])
+});
 
 const months = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'];
 const today = new Date().toISOString();
