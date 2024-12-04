22741cc4afc26a8f0c6d2b4222ad8ee3c3ffbb5e
Shelley Vohr <shelley.vohr@gmail.com>
2024-12-04 16:07:54 +0100

    refactor: download stats programmatically

---
+++ fetch-data.js
@@ -0,0 +1,64 @@
+const fs = require('fs');
+const path = require('path');
+const { Octokit } = require('@octokit/rest');
+const { GITHUB_TOKEN } = process.env;
+
+const packages = [
+  'electron',
+  'electron-prebuilt',
+  'electron-nightly',
+  'electron-prebuilt-compile'
+];
+
+async function fetchReleases() {
+  const outputPath = path.join(__dirname, 'releases.json');
+  const octokit = new Octokit({ auth: GITHUB_TOKEN });
+
+  let page = 0;
+  const allReleases = [];
+
+  while (true) {
+    const { data } = await octokit.rest.repos.listReleases({
+      owner: 'electron',
+      repo: 'electron',
+      per_page: 100,
+      page,
+    });
+
+    if (data.length === 0) break;
+
+    allReleases.push(...data);
+    page++;
+  }
+
+  fs.writeFileSync(outputPath, JSON.stringify(allReleases, null, 2), 'utf8');
+}
+
+async function fetchStatistics(pkgs = packages) {
+  const nextYear = `${new Date().getFullYear() + 1}-01-01`;
+
+  if (!Array.isArray(pkgs)) {
+    console.error('Packages must be an array');
+    process.exit(1);
+  }
+
+  for (const package of pkgs) {
+    const url = `https://api.npmjs.org/downloads/range/2016-07-26:${nextYear}/${package}`;
+    const outputPath = path.join(__dirname, 'data', `${package}.json`);
+
+    try {
+      const rawData = await fetch(url);
+      const data = await rawData.json();
+      fs.writeFileSync(outputPath, JSON.stringify(data, null, 2), 'utf8');
+      console.log(`${package} download statistics written to ${outputPath}`);
+    } catch (e) {
+      console.error('Error fetching data:', error);
+      process.exit(1);
+    }
+  }
+}
+
+module.exports = {
+  fetchStatistics,
+  fetchReleases
+}
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const requireDir = require('require-dir');
-const data = requireDir('./data');
+const { fetchStatistics, fetchReleases } = require('./fetch-data');
+
+const months = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'];
 
 // Generates an array of years ranging from 2015 - present year.
 const generateYearRange = () => {
@@ -9,50 +11,66 @@ const generateYearRange = () => {
   }, (_, i) => 2015 + i);
 };
 
-let combined = Object.keys(data).reduce((acc, key) => {
-  data[key].downloads.forEach(({ day, downloads }) => {
-    acc[day] = (acc[day] || 0) + downloads;
+function calculateStatistics() {
+  const data = requireDir('./data');
+
+  let combined = Object.keys(data).reduce((acc, key) => {
+    data[key].downloads.forEach(({ day, downloads }) => {
+      acc[day] = (acc[day] || 0) + downloads;
+    });
+    return acc;
+  }, {});
+
+  combined = Object.entries(combined).sort((a, b) => {
+    a[0].localeCompare(b[0])
   });
-  return acc;
-}, {});
 
-combined = Object.entries(combined).sort((a, b) => {
-  a[0].localeCompare(b[0])
-});
+  const today = new Date().toISOString();
+  const results = [];
 
-const months = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'];
-const today = new Date().toISOString();
-const results = [];
+  generateYearRange().forEach((year) => {
+    months.forEach((month) => {
+      const yearAndMonth = `${year}-${month}`;
+      const downloadsInMonth = combined
+        .filter((day) => day[0].startsWith(yearAndMonth))
+        .map((day) => day[1])
+        .reduce((sum, value) => sum + value, 0);
 
-generateYearRange().forEach((year) => {
-  months.forEach((month) => {
-    const yearAndMonth = `${year}-${month}`;
-    const downloadsInMonth = combined
-      .filter((day) => day[0].startsWith(yearAndMonth))
-      .map((day) => day[1])
-      .reduce((sum, value) => sum + value, 0);
+      // Account for this month, which is still in progress
+      const daysInMonth = yearAndMonth === today.substring(0, 7) ? Number(today.substring(8, 2)) : 30;
+      const average = Math.round(downloadsInMonth / daysInMonth);
 
-    // Account for this month, which is still in progress
-    const daysInMonth = yearAndMonth === today.substring(0, 7) ? Number(today.substring(8, 2)) : 30;
-
-    const average = Math.round(downloadsInMonth / daysInMonth);
-
-    if (average > 0) {
-      results.push([yearAndMonth, average]);
-    }
+      if (average > 0) {
+        results.push([yearAndMonth, average]);
+      }
+    });
   });
-});
 
-console.log('# Electron Download Stats\n');
-console.log(
-  '[![Update Download Stats](https://github.com/electron/download-stats/actions/workflows/schedule.yml/badge.svg)](https://github.com/electron/download-stats/actions/workflows/schedule.yml)\n',
-);
-console.log('## npm Downloads\n');
-console.log(
-  '> Average combined daily downloads by month for `electron`, `electron-prebuilt`, `electron-nightly`, and `electron-prebuilt-compile`.\n',
-);
-console.log('Month | Daily Downloads');
-console.log('--- | ---');
-results.forEach((result) => {
-  console.log(result.join(' | '));
-});
+  return results;
+}
+
+function writeToReadme(results) {
+  console.log('# Electron Download Stats\n');
+  console.log(
+    '[![Update Download Stats](https://github.com/electron/download-stats/actions/workflows/schedule.yml/badge.svg)](https://github.com/electron/download-stats/actions/workflows/schedule.yml)\n',
+  );
+  console.log('## npm Downloads\n');
+  console.log(
+    '> Average combined daily downloads by month for `electron`, `electron-prebuilt`, `electron-nightly`, and `electron-prebuilt-compile`.\n',
+  );
+  console.log('Month | Daily Downloads');
+  console.log('--- | ---');
+  results.forEach((result) => {
+    console.log(result.join(' | '));
+  });
+}
+
+async function updateStatistics() {
+  await fetchStatistics();
+  await fetchReleases();
+
+  const results = calculateStatistics();
+  writeToReadme(results);
+}
+
+updateStatistics();
